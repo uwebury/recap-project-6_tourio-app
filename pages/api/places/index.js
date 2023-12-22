f88a4303fec57cbcea6ceb2ddf27104cca4b260b
uwebury <142432719+uwebury@users.noreply.github.com>
2023-12-22 15:18:21 +0100

    Read Data

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,13 @@
-import { places } from "../../../lib/db";
+import dbConnect from "@/db/connect";
+import Place from "@/db/models/Places";
 
-export default function handler(request, response) {
-  return response.status(200).json(places);
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const places = await Place.find();
+    return response.status(200).json(places);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  }
 }
