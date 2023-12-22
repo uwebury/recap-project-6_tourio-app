cc9f7fbb284fcb56c724c4b0b8aa8d5d7bc0a339
uwebury <142432719+uwebury@users.noreply.github.com>
2023-12-22 16:51:47 +0100

    Create from updated

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -4,10 +4,20 @@ import Place from "@/db/models/Places";
 export default async function handler(request, response) {
   await dbConnect();
 
-  if (request.method === "GET") {
-    const places = await Place.find();
-    return response.status(200).json(places);
-  } else {
-    return response.status(405).json({ message: "Method not allowed" });
+  try {
+    if (request.method === "GET") {
+      const places = await Place.find();
+      return response.status(200).json(places);
+    }
+    if (request.method === "POST") {
+      const placeData = request.body;
+      await Place.create(placeData);
+      return response.status(201).json({ status: "Place added" });
+    } else {
+      return response.status(405).json({ message: "Method not allowed" });
+    }
+  } catch (error) {
+    console.error(error);
+    return response.status(400).json({ error: error.message });
   }
 }
